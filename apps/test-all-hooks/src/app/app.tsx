52d978f71ed21a5445d9023adb22966dab8566d5
hakobpogh <hakobpogh@gmail.com>
2022-07-28 12:54:08 +0400

    feat: add test case in test-all-hooks app

--- apps/test-all-hooks/src/app/app.tsx
+++ apps/test-all-hooks/src/app/app.tsx
@@ -1,14 +1,28 @@
 import styled from '@emotion/styled';
-import NxWelcome from './nx-welcome';
+import { useImmediateEffect } from '@react-hookbox/use-immediate-effect';
+import { useEffect, useState } from 'react';
 
 const StyledApp = styled.div`
   // Your style here
 `;
 
 export function App() {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    console.log('useEffect', `count=${count}`, 'runs after rendering');
+  }, [count]);
+
+  useImmediateEffect(() => {
+    console.log('useImmediateEffect', `count=${count}`, 'runs during rendering');
+  }, [count]);
+
+  console.log('render', `count=${count}`);
+
   return (
     <StyledApp>
-      <NxWelcome title="test-all-hooks" />
+      <div>{count}</div>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
     </StyledApp>
   );
 }
